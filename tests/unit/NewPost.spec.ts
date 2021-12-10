e4a7ba2fe55ddb929661c6251b57c7d22ed077f9
devabhimanyu <dev.chauhan.singh.abhimanyu@gmail.com>
2021-12-10 20:42:10 +0530

    test: 💍 NewPost test added, vue-router mocked

---
+++ tests/unit/NewPost.spec.ts
@@ -0,0 +1,60 @@
+import { mount } from "@vue/test-utils";
+import NewPost from "../../src/pages/NewPost.vue";
+
+import { Store } from "../../src/store";
+
+let routes: string[] = [];
+
+jest.mock("vue-router", () => ({
+  useRouter: () => {
+    return {
+      push: (route: string) => {
+        routes.push(route);
+      },
+    };
+  },
+}));
+
+jest.mock("axios", () => ({
+  post: (url: string, payload: any) => {
+    return {
+      data: payload,
+    };
+  },
+}));
+
+describe("NewPost Component", () => {
+  const store = new Store({
+    posts: {
+      all: new Map(),
+      ids: [],
+      loaded: false,
+    },
+    authors: {
+      ids: ["100"],
+      all: new Map([["100", { username: "username", id: "100" }]]),
+      loaded: true,
+      currentUserId: "100",
+    },
+  });
+
+  beforeEach(() => {
+    routes = [];
+  });
+
+  it.only("creates a post and redirects to /", async () => {
+    const wrapper = mount(NewPost, {
+      global: {
+        plugins: [store],
+      },
+    });
+
+    expect(store.getState().posts.ids).toHaveLength(0);
+
+    await wrapper.find('[data-test="save-btn"]').trigger("click");
+
+    expect(store.getState().posts.ids).toHaveLength(1);
+
+    expect(routes).toEqual(["/"]);
+  });
+});
